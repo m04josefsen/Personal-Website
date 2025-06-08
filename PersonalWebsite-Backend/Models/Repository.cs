63ca844cbf32f8a587941b9f76b2ab07e5ca415c
Marco Josefsen <majos6959@oslomet.no>
2025-06-08 17:20:54 +0200

    Created Repository model class

---
+++ PersonalWebsite-Backend/Models/Repository.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+
+namespace PersonalWebsite_Backend.Models;
+
+public class Repository
+{
+    // From /repos/{owner}/{repo}
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public string Description { get; set; }
+
+    // From /repos/{owner}/{repo}/languages
+    public ArrayList Languages { get; set; }
+    
+    class Language
+    {
+        public String Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
